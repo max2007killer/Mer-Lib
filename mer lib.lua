6ff58382439925c6756106e280bcc698472d7132
max2007killer <50517869+max2007killer@users.noreply.github.com>
2022-10-27 22:19:34 +0300

    Update mer lib.lua

--- mer lib.lua
+++ mer lib.lua
@@ -2822,7 +2822,7 @@ function Library:credit(options)
 				Size = UDim2.fromOffset(24, 24),
 				Position = UDim2.new(1, -40, 1, -8),
 				Theme = {BackgroundColor3 = {"Main", 10}}
-			}):round(5):tooltip("copy v3rm")
+			}):round(5):tooltip("copy discord server")
 			local v3rmillion = v3rmillionContainer:object("ImageLabel", {
 				Image = "http://www.roblox.com/asset/?id=8594086769",
 				Size = UDim2.new(1, -4, 1, -4),
